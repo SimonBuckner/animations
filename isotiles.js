4f9be74482b1527237abafcb4ba0d6f034d2fb82
Simon Buckner <simonbuckner@hotmail.com>
2022-03-05 12:13:18 +0000

    Add tile pattern base on SIN and fade effect.

--- isotiles.js
+++ isotiles.js
@@ -11,26 +11,23 @@ const settings = {
 };
 
 const params = {
-  tiles: Array(200),
-  num_tiles: 200,
+  tiles: [],
+  rows: 31,
+  cols: 15,
   y_speed: 0.1,
   y_offset: 0,
   y_bump: 1,
   color: 0,
   c_speed: 5,
+  s_speed: 4,
+  z_speed: 1,
+  begin_fade: 15,
+  full_fade: 25,
 };
 
 const sketch = () => {
-
-  for (let i = params.num_tiles - 1; i >= 0; i--) {
-    const x = i % 5;
-    const y = Math.floor(i / 5);
-    params.tiles.push(new Tile(params.color));
-    console.log(x);
-    if (x == 0) {
-      if (params.color <= 0 || params.color >= 255) { params.c_speed *= -1; }
-    };
-  };
+  const num_tiles = params.rows * params.cols;
+  params.tiles = Array(num_tiles);
 
   return ({ context, width, height, frame }) => {
     context.fillStyle = 'white';
@@ -40,33 +37,55 @@ const sketch = () => {
 
     if (params.y_offset > params.y_bump) {
       params.y_offset = 0;
-      bump_tiles();
+      bump_tiles(frame);
     };
 
-    for (let i = 0; i < params.num_tiles; i++) {
-      const x = i % 5;
-      const y = Math.floor(i / 5);
-      params.tiles[i].draw(context, width + 100, 150, x, y + params.y_offset);
+    const num_tiles = params.rows * params.cols;
+
+    for (let i = 0; i < num_tiles; i++) {
+      const x = i % params.cols;
+      const y = Math.floor(i / params.cols);
+      if (params.tiles[i]) {
+        params.tiles[i].draw(context, width + 100, 150, x, y + params.y_offset);
+      }
     };
   };
 };
 
-const bump_tiles = (tiles) => {
-  const keep_rows = params.num_tiles - (params.num_tiles % 5);
+const bump_tiles = (frame) => {
+  const num_tiles = params.rows * params.cols;
+
+  const keep_rows = num_tiles - (num_tiles % params.cols);
   for (let i = keep_rows - 1; i >= 0; i--) {
-    params.tiles[i + 5] = params.tiles[i];
+    params.tiles[i + params.cols] = params.tiles[i];
   };
 
   params.color += params.c_speed;
+  const z = Math.sin(math.degToRad(frame * params.z_speed)); // * 40;
+
   if (params.color <= 0 || params.color >= 255) { params.c_speed *= -1; }
-  for (let i = 4; i >= 0; i--) {
-    params.tiles[i] = new Tile(params.color);
+
+  const s = Math.sin(math.degToRad(frame * params.s_speed))
+  const s1 = Math.round(math.mapRange(s, -1, 1, 0, params.cols - 1));
+  const s2 = params.cols - s1 - 1;
+
+  for (let i = params.cols - 1; i >= 0; i--) {
+    if (i >= s1 && i <= s2) {
+      params.tiles[i] = new Tile(255 - params.color, z * 40);
+    } else if (i >= s2 && i <= s1) {
+      params.tiles[i] = new Tile(255 - params.color, z * 40);
+    } else {
+      params.tiles[i] = new Tile(params.color, z * 40);
+    };
   };
 };
 
 class Tile {
-  constructor(c) {
+  constructor(c, z) {
     this.c = c;
+    this.z = z;
+    // this.z = random.range(-10, 10);
+
   }
 
   draw(context, ox, oy, tx, ty) {
@@ -88,8 +107,20 @@ class Tile {
     const ysb = 10;   // side bottom
     const ybb = 35;   // bottom bottom
 
+    let alpha = 1.0;
+    const range = params.full_fade - params.begin_fade;
+    const ay = ty - params.begin_fade;
+
+    if (ty >= params.begin_fade) {
+      alpha = 1 - (ay / range);
+    }
+    if (ty >= params.full_fade) {
+      alpha = 0;
+    }
     context.save();
+    context.globalAlpha = alpha;
     context.translate(x, y);
+    context.translate(0, this.z);
 
     // sides of tile
     context.beginPath();
@@ -134,24 +165,10 @@ const createPane = () => {
   folder = pane.addFolder({ title: "Tiles" });
   folder.addInput(params, "y_speed", { min: 0.01, max: 1, step: 0.01 });
   folder.addInput(params, "c_speed", { min: 1, max: 255, step: 1 });
-  // folder.addInput(params, "rows", {min: 2, max: 50, step: 1});
-  // folder.addInput(params, "scaleMin", {min: 1, max: 100});
-  // folder.addInput(params, "scaleMax", {min: 1, max: 100});
-
-  // folder = pane.addFolder({ title: "Noise"});
-  // folder.addInput(params, "freq", {min: -0.01, max: 0.01});
-  // folder.addInput(params, "amp", {min: 0, max: 1});
-  // folder.addInput(params, "animate");
-  // folder.addInput(params, "frame",{min: 1, max: 999});
-
-
-  // tiles: Array(200),
-  // num_tiles: 200,
-  // y_speed: 0.1,
-  // y_offset: 0,
-  // y_bump: 1,
-  // color: 0,
-  // c_speed: 5,
+  folder.addInput(params, "z_speed", { min: 1, max: 255, step: 1 });
+  folder.addInput(params, "s_speed", { min: 1, max: 8, step: 0.1 });
+  folder.addInput(params, "begin_fade", { min: 1, max: 50, step: 1 });
+  folder.addInput(params, "full_fade", { min: 1, max: 50, step: 1 });
 };
 
 createPane();
